10d90063b0906ba113801055fd80d0b63f18cf93
Tomassz <tomasz.pogorzelski@neoteric.eu>
2020-01-15 22:54:34 +0100

    chore(*): add missing field

--- src/modules/balance/balance.dto.ts
+++ src/modules/balance/balance.dto.ts
@@ -1,4 +1,7 @@
+import { BalanceType } from './enums/BalanceType.enum';
+
 export class CreateBalanceDto {
   public readonly source: string;
   public readonly amount: number;
+  public readonly type: BalanceType;
 }
--- src/modules/balance/balance.schema.ts
+++ src/modules/balance/balance.schema.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 
 export const BalanceSchema = new mongoose.Schema({
   id: String,
+  type: String,
   source: String,
   amount: Number,
 });
